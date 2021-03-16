f63aa3fcda3ba869c0be006cdec4829057332c2f
Kim Duer <kimcodesjs@gmail.com>
2021-03-16 18:41:56 -0500

    continued routing setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import firebase from 'firebase'
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDKWIvmbNuDngSPMtXPavocpSFLcswEglc",
+    authDomain: "serenity-ignited.firebaseapp.com",
+    projectId: "serenity-ignited",
+    storageBucket: "serenity-ignited.appspot.com",
+    messagingSenderId: "178174477632",
+    appId: "1:178174477632:web:c433d0e213435a6f06e6a5",
+    measurementID: "G-KQ2T23DTYX"
+})
 
 
 ReactDOM.render(
