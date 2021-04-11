0774263ced5bc15657143ef9261435e0cce5d1ec
Kim Duer <kimcodesjs@gmail.com>
2021-04-11 13:59:21 -0500

    created booking page, added reiki info

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './Components/Header'
 import Welcome from './Components/Welcome'
 import SoapShop from './Components/SoapShop/SoapShop'
 import ReikiShop from './Components/ReikiShop/ReikiShop'
+import Booking from './Components/ReikiShop/Booking'
 import ContactMe from './Components/ContactMe'
 import AboutMe from './Components/AboutMe'
 import Admin from './Components/Admin/Admin'
@@ -23,6 +24,7 @@ const App = () => {
                 <Switch>
                     <Route path="/" component={Welcome} exact={true}/>   
                     <Route path="/serenity-in-soap" component={SoapShop}/>
+                    <Route path='/serenity-in-healing/booking' component={Booking} />
                     <Route path="/serenity-in-healing" component={ReikiShop}/>
                     <Route path="/contact-me" component={ContactMe}/>
                     <Route path="/about-me" component={AboutMe}/>
