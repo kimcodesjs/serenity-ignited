0deb46d531dc4972d459fd23cd1e4dc26845d08a
Kim Duer <kimcodesjs@gmail.com>
2021-10-12 20:04:47 -0500

    building out scheduler component

--- src/Booking/Scheduler.js
+++ src/Booking/Scheduler.js
@@ -1,32 +1,70 @@
 import React, { useEffect, useState } from 'react'
 import { createUseStyles } from 'react-jss'
 import Calendar from 'react-calendar'
+import { DateTime, Duration } from 'luxon'
+
 
 // to do - styling calendar? create timeslot picker
+const useStyles = createUseStyles({
+    container: {
+        width: '100%',
+        justifyContent: 'center',
+        //alignItems: 'center',
+        display: 'inline-flex'
+    },
+    timeSlots: {
+        paddingLeft: '10px'
+    },
+    calendar: {
+        
+    }
+    
+})
+const Scheduler = ({ setSchedule, session }) => {
 
-const Scheduler = ({ setSchedule }) => {
+    let classes = useStyles()
 
     let [activeDate, setActiveDate] = useState(null)
 
     useEffect(() => {
         console.log(activeDate)
+        createTimeSlots(activeDate, session)
     }, [activeDate])
     
     const updateActiveDate = (value) => {
-        setActiveDate(value.toDateString())
+        const date = DateTime.fromJSDate(value)
+        setActiveDate(date.toLocaleString(DateTime.DATE_HUGE))
+    }
+
+    const createTimeSlots = (date, session) => {
+        // date parameter should be a luxon DateTime object
+        const buffer = Duration.fromObject({ minutes: 15})
+        
+        // read session type and apply appropriate duration
+        // why does this console.log session object and not just session duration? 
+        const duration = session.duration
+        console.log(buffer, duration)
+        
+        // generate available time slots based on practitioner availability, including buffer time
     }
     return (
         <>
             <h3>When would you like to receive your healing?</h3>
             <br />
-                <Calendar 
+            <div className={classes.container}>
+            <div className={classes.calendar}>
+                <Calendar
                     calendarType='US'
                     onChange={updateActiveDate}
-                    minDate={new Date()}/>
+                    minDate={new Date()}
+                />
+            </div>
 
-                <span>{activeDate}</span>
-                <span></span>
-            
+                <div className={classes.timeSlots}>
+                    <h3>{activeDate ? activeDate : DateTime.now().toLocaleString(DateTime.DATE_HUGE)}</h3>
+                    
+                </div>
+            </div>
         </>
     )
 }
