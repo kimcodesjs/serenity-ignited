8b9f739d0f827832d56b228017dc262a80bb2863
Kim Duer <kimcodesjs@gmail.com>
2021-10-12 20:03:27 -0500

    update session object & OC props

--- src/Booking/Connection.js
+++ src/Booking/Connection.js
@@ -9,7 +9,7 @@ const Connection = ({ setConnection, updateView, session }) => {
 
     
     const onConfirm = () => {
-        setConnection(active)
+        setConnection(active.id)
         updateView(3)
     }
 
@@ -36,14 +36,14 @@ const Connection = ({ setConnection, updateView, session }) => {
             <h3>Select how you would prefer to connect:</h3>
             {session === 'Sample Session' || session === 'General Healing' || session === 'In-Depth Healing' ?
                 <div> 
-                    <OptionCard option={options.inPerson} setActive={setActive} active={active === options.inPerson.id ? true : false} />
-                    <OptionCard option={options.remotePhone} setActive={setActive} active={active === options.remotePhone.id ? true : false} />
-                    <OptionCard option={options.remoteVideo} setActive={setActive} active={active === options.remoteVideo.id ? true : false} />
-                    <OptionCard option={options.remote} setActive={setActive} active={active === options.remote.id ? true : false} />
+                    <OptionCard option={options.inPerson} setActive={setActive} active={active && active.id === options.inPerson.id ? true : false} />
+                    <OptionCard option={options.remotePhone} setActive={setActive} active={active && active.id === options.remotePhone.id ? true : false} />
+                    <OptionCard option={options.remoteVideo} setActive={setActive} active={active && active.id === options.remoteVideo.id ? true : false} />
+                    <OptionCard option={options.remote} setActive={setActive} active={active && active.id === options.remote.id ? true : false} />
                 </div>
                 : 
                 <div>
-                    <OptionCard option={options.inPerson} setActive={setActive} active={active === options.inPerson.id ? true : false} />
+                    <OptionCard option={options.inPerson} setActive={setActive} active={active && active.id === options.inPerson.id ? true : false} />
                 </div>}
             <button onClick={onConfirm}>Continue</button>
         </>
--- src/Booking/Session.js
+++ src/Booking/Session.js
@@ -8,56 +8,64 @@ const Session = ({ setSession, updateView }) => {
 
     const onConfirm = () => {
         setSession(active)
+        console.log(active)
         updateView(2)
     }
 
+    // Session Objects - each contain all info for each session
     const options = {
         sampleSession: {
             id: 'Sample Session',
             description: 'A short sample session for those who are interested in testing the waters without committing to a full price session.',
+            duration: { minutes: 15 },
             price: 20
         },
         generalHealing: {
             id: 'General Healing',
             description: 'A general cleansing session for those in need of a little more than what the sample session offers.',
+            duration: { minutes: 30},
             price: 40
         },
         inDepth: {
             id: 'In-Depth Healing',
             description: 'An extensive healing session, typically useful in targeting a specific ailment.',
+            duration: { hours: 1 },
             price: 65
         },
         accessBars: {
             id: 'Access Bars',
             description: 'A full-length Access Bars session to clear energy blockages in your consciousness.',
+            duration: { hours: 1, minutes: 30 },
             price: 150
         },
         accessPartial: {
             id: 'Partial Access Bars',
             description: 'A shorter Access Bars session.',
+            duration: { minutes: 45 },
             price: 75
         },
         package1: {
             id: 'Reiki + Access Bars',
             description: 'A powerful combo session of Reiki and Access Bars healing.',
+            duration: { minutes: 45 },
             price: 75
         }
     }
-
+    // When OptionCard is clicked, Booking.js receives the active session object
     return (
         <>
             <h3>Choose your healing session:</h3> 
-
+            
             <h4>Reiki</h4>
-                <OptionCard option={options.sampleSession} setActive={setActive} active={active === options.sampleSession.id ? true : false} />
-                <OptionCard option={options.generalHealing} setActive={setActive} active={active === options.generalHealing.id ? true : false} />
-                <OptionCard option={options.inDepth} setActive={setActive} active={active === options.inDepth.id ? true : false} />
+                <OptionCard option={options.sampleSession} setActive={setActive} active={active && active.id === options.sampleSession.id ? true : false} />
+                <OptionCard option={options.generalHealing} setActive={setActive} active={active && active.id === options.generalHealing.id ? true : false} />
+                <OptionCard option={options.inDepth} setActive={setActive} active={active && active.id === options.inDepth.id ? true : false} />
             
             <h4>Access Consciousness</h4>
-                <OptionCard option={options.accessBars} setActive={setActive} active={active === options.accessBars.id ? true : false} />
+                <OptionCard option={options.accessBars} setActive={setActive} active={active && active.id === options.accessBars.id ? true : false} />
             
             <h4>Packages</h4>
-                <OptionCard option={options.package1} setActive={setActive} active={active === options.package1.id ? true : false} />
+                <OptionCard option={options.package1} setActive={setActive} active={active && active.id === options.package1.id ? true : false} />
 
             <button onClick={onConfirm}>continue</button>
         </>
