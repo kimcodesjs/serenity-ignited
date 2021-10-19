afbf03c1f8b37883e26500f8ca2344fae8b328cf
Kim Duer <kimcodesjs@gmail.com>
2021-10-19 17:27:15 -0500

    built out createTimeSlots logic and display

--- src/Booking/Scheduler.js
+++ src/Booking/Scheduler.js
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { createUseStyles } from 'react-jss'
 import Calendar from 'react-calendar'
-import { DateTime, Duration } from 'luxon'
+import { DateTime, Duration, Interval } from 'luxon'
 
 
-// to do - styling calendar? create timeslot picker
 const useStyles = createUseStyles({
     container: {
         width: '100%',
         justifyContent: 'center',
-        //alignItems: 'center',
         display: 'inline-flex'
     },
     timeSlots: {
@@ -20,49 +18,64 @@ const useStyles = createUseStyles({
     }
     
 })
-const Scheduler = ({ setSchedule, session }) => {
+const Scheduler = ({ setSchedule, duration }) => {
 
     let classes = useStyles()
 
-    let [activeDate, setActiveDate] = useState(null)
+    let [activeDate, setActiveDate] = useState(DateTime.now())
+    let [timeslotsUI, setTimeslotsUI] = useState([])
 
     useEffect(() => {
-        console.log(activeDate)
-        createTimeSlots(activeDate, session)
+        // Existing bookings to be fetched from database
+        let currentBookings = [
+            Interval.fromDateTimes(DateTime.fromISO("2021-10-18T18:30:00"), DateTime.fromISO("2021-10-18T18:45:00").plus({ minutes: 15 }))
+        ]
+        // Working hours to be fetched from database
+        let workingHours = Interval.fromDateTimes(activeDate.set({hour: 18, minute: 0, second: 0}), activeDate.set({hour: 21, minute:0, second: 0}))
+        createTimeSlots(duration, currentBookings, workingHours)
+
     }, [activeDate])
     
     const updateActiveDate = (value) => {
-        const date = DateTime.fromJSDate(value)
-        setActiveDate(date.toLocaleString(DateTime.DATE_HUGE))
+        setActiveDate(DateTime.fromJSDate(value))
     }
 
-    const createTimeSlots = (date, session) => {
-        // date parameter should be a luxon DateTime object
-        const buffer = Duration.fromObject({ minutes: 15})
-        
-        // read session type and apply appropriate duration
-        // why does this console.log session object and not just session duration? 
-        const duration = session.duration
-        console.log(buffer, duration)
+    // createTimeSlots 
+    const createTimeSlots = (duration, bookings, workingHours) => {
+
+        const sessionLength = Duration.fromObject(duration)
         
         // generate available time slots based on practitioner availability, including buffer time
+        const timeslots = workingHours.splitBy(sessionLength).filter((timeslot) => {
+            if (!timeslot.overlaps(bookings[0])) {
+                return timeslot
+            }
+        })
+        
+        //format timeslots as UI
+        const timeslotsUI = timeslots.map((timeslot) => {
+            return (<li key={timeslot.start.toString()}>
+                {timeslot.start.toLocaleString(DateTime.TIME_SIMPLE)} - {timeslot.end.toLocaleString(DateTime.TIME_SIMPLE)}
+            </li>)
+        })
+        setTimeslotsUI(timeslotsUI)
     }
     return (
         <>
             <h3>When would you like to receive your healing?</h3>
             <br />
             <div className={classes.container}>
-            <div className={classes.calendar}>
-                <Calendar
-                    calendarType='US'
-                    onChange={updateActiveDate}
-                    minDate={new Date()}
-                />
-            </div>
+                <div className={classes.calendar}>
+                    <Calendar
+                        calendarType='US'
+                        onChange={updateActiveDate}
+                        minDate={new Date()}
+                    />
+                </div>
 
-                <div className={classes.timeSlots}>
-                    <h3>{activeDate ? activeDate : DateTime.now().toLocaleString(DateTime.DATE_HUGE)}</h3>
-                    
+                <div className={classes.timeSlots}> 
+                    <h3>{activeDate.toLocaleString(DateTime.DATE_HUGE)}</h3>
+                    <ul>{timeslotsUI}</ul>
                 </div>
             </div>
         </>
