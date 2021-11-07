e624a2e75be34cc8fbb3b10d1fb0a84025c5b09b
Kim Duer <kimcodesjs@gmail.com>
2021-11-06 23:40:55 -0500

    add onConfirm, map timeslots ui inline

--- src/Booking/Scheduler.js
+++ src/Booking/Scheduler.js
@@ -12,23 +12,22 @@ const useStyles = createUseStyles({
     },
     timeSlots: {
         paddingLeft: '10px'
-    },
-    calendar: {
-        
     }
     
 })
-const Scheduler = ({ setSchedule, duration }) => {
+const Scheduler = ({ setSchedule, updateView, duration }) => {
 
     let classes = useStyles()
 
     let [activeDate, setActiveDate] = useState(DateTime.now())
-    let [timeslotsUI, setTimeslotsUI] = useState([])
+    let [activeTimeslot, setActiveTimeslot] = useState(null)
+    let [timeslots, setTimeslots] = useState([])
 
     useEffect(() => {
         // Existing bookings to be fetched from database
         let currentBookings = [
-            Interval.fromDateTimes(DateTime.fromISO("2021-10-18T18:30:00"), DateTime.fromISO("2021-10-18T18:45:00").plus({ minutes: 15 }))
+            Interval.fromDateTimes(DateTime.fromISO("2021-11-19T18:30:00"), DateTime.fromISO("2021-11-19T18:45:00").plus({ minutes: 15 })),
+            //Interval.fromDateTimes(DateTime.fromISO("2021-11-19T19:30:00"), DateTime.fromISO("2021-11-19T19:45:00").plus({ minutes: 15 }))
         ]
         // Working hours to be fetched from database
         let workingHours = Interval.fromDateTimes(activeDate.set({hour: 18, minute: 0, second: 0}), activeDate.set({hour: 21, minute:0, second: 0}))
@@ -39,27 +38,36 @@ const Scheduler = ({ setSchedule, duration }) => {
     const updateActiveDate = (value) => {
         setActiveDate(DateTime.fromJSDate(value))
     }
-
+    
     // createTimeSlots 
     const createTimeSlots = (duration, bookings, workingHours) => {
 
         const sessionLength = Duration.fromObject(duration)
-        
-        // generate available time slots based on practitioner availability, including buffer time
+       /* const timeslots = workingHours.splitBy(sessionLength)
+        const availableSlots = []
+        // generate available time slots and filter out any that overlap w/ existing bookings
+        bookings.forEach((booking) => {
+
+        })*/
         const timeslots = workingHours.splitBy(sessionLength).filter((timeslot) => {
-            if (!timeslot.overlaps(bookings[0])) {
-                return timeslot
-            }
+            
+                if (!timeslot.overlaps(bookings[0])) {
+                    return timeslot
+                }
+            
         })
         
-        //format timeslots as UI
-        const timeslotsUI = timeslots.map((timeslot) => {
-            return (<li key={timeslot.start.toString()}>
-                {timeslot.start.toLocaleString(DateTime.TIME_SIMPLE)} - {timeslot.end.toLocaleString(DateTime.TIME_SIMPLE)}
-            </li>)
-        })
-        setTimeslotsUI(timeslotsUI)
+        setTimeslots(timeslots)
     }
+
+    const onConfirm = () => {
+        setSchedule({
+            date: activeDate,
+            time: activeTimeslot
+        })
+        updateView(4)
+    }
+    // how do I get Timeslots to reset when activeDate changes? 
     return (
         <>
             <h3>When would you like to receive your healing?</h3>
@@ -75,8 +83,22 @@ const Scheduler = ({ setSchedule, duration }) => {
 
                 <div className={classes.timeSlots}> 
                     <h3>{activeDate.toLocaleString(DateTime.DATE_HUGE)}</h3>
-                    <ul>{timeslotsUI}</ul>
+
+                    <select onChange={(e) => {e.preventDefault; setActiveTimeslot(Interval.fromISO(e.target.value))}} defaultValue='default'>
+                        <option disabled hidden value='default'>Please select a timeslot.</option>
+                        {
+                            timeslots.map((timeslot, index) => {
+                                return (
+                                    <option key={index} value={timeslot.toISO()}>
+                                        {timeslot.start.toLocaleString(DateTime.TIME_SIMPLE)} - {timeslot.end.toLocaleString(DateTime.TIME_SIMPLE)}
+                                    </option>
+                                )
+                            })    
+                        }
+                    </select>
+                    <button onClick={onConfirm}>Continue</button>
                 </div>
+                
             </div>
         </>
     )
