1262984b38e6e996f4b29a99d4ca45b87913ec6b
Kim Duer <kimcodesjs@gmail.com>
2021-11-22 17:53:33 -0600

    fixed timeslot filter

--- src/Booking/Scheduler.js
+++ src/Booking/Scheduler.js
@@ -3,7 +3,6 @@ import { createUseStyles } from 'react-jss'
 import Calendar from 'react-calendar'
 import { DateTime, Duration, Interval } from 'luxon'
 
-
 const useStyles = createUseStyles({
     container: {
         width: '100%',
@@ -21,13 +20,15 @@ const Scheduler = ({ setSchedule, updateView, duration }) => {
 
     let [activeDate, setActiveDate] = useState(DateTime.now())
     let [activeTimeslot, setActiveTimeslot] = useState(null)
-    let [timeslots, setTimeslots] = useState([])
+    let [availableTimeslots, setTimeslots] = useState([])
 
     useEffect(() => {
         // Existing bookings to be fetched from database
         let currentBookings = [
             Interval.fromDateTimes(DateTime.fromISO("2021-11-19T18:30:00"), DateTime.fromISO("2021-11-19T18:45:00").plus({ minutes: 15 })),
-            //Interval.fromDateTimes(DateTime.fromISO("2021-11-19T19:30:00"), DateTime.fromISO("2021-11-19T19:45:00").plus({ minutes: 15 }))
+            Interval.fromDateTimes(DateTime.fromISO("2021-11-19T19:30:00"), DateTime.fromISO("2021-11-19T19:45:00").plus({ minutes: 15 }))
+            // 
+            // 
         ]
         // Working hours to be fetched from database
         let workingHours = Interval.fromDateTimes(activeDate.set({hour: 18, minute: 0, second: 0}), activeDate.set({hour: 21, minute:0, second: 0}))
@@ -41,23 +42,23 @@ const Scheduler = ({ setSchedule, updateView, duration }) => {
     
     // createTimeSlots 
     const createTimeSlots = (duration, bookings, workingHours) => {
-
         const sessionLength = Duration.fromObject(duration)
-       /* const timeslots = workingHours.splitBy(sessionLength)
-        const availableSlots = []
-        // generate available time slots and filter out any that overlap w/ existing bookings
-        bookings.forEach((booking) => {
-
-        })*/
-        const timeslots = workingHours.splitBy(sessionLength).filter((timeslot) => {
-            
-                if (!timeslot.overlaps(bookings[0])) {
-                    return timeslot
+        const timeslots = workingHours.splitBy(sessionLength)
+        const filteredTimeslots = []
+        for(let i=0;i<timeslots.length;i++){
+            for(let j=0;j<bookings.length;j++){
+                if (timeslots[i].overlaps(bookings[j])) {
+                    break;   
                 }
-            
-        })
-        
-        setTimeslots(timeslots)
+                else {
+                    if(j === bookings.length -1){
+                        filteredTimeslots.push(timeslots[i]);
+                    }
+                }
+            }
+        }
+        // console.log(filteredTimeslots)
+        setTimeslots(filteredTimeslots)
     }
 
     const onConfirm = () => {
@@ -67,6 +68,7 @@ const Scheduler = ({ setSchedule, updateView, duration }) => {
         })
         updateView(4)
     }
+
     // how do I get Timeslots to reset when activeDate changes? 
     return (
         <>
@@ -87,7 +89,7 @@ const Scheduler = ({ setSchedule, updateView, duration }) => {
                     <select onChange={(e) => {e.preventDefault; setActiveTimeslot(Interval.fromISO(e.target.value))}} defaultValue='default'>
                         <option disabled hidden value='default'>Please select a timeslot.</option>
                         {
-                            timeslots.map((timeslot, index) => {
+                            availableTimeslots.map((timeslot, index) => {
                                 return (
                                     <option key={index} value={timeslot.toISO()}>
                                         {timeslot.start.toLocaleString(DateTime.TIME_SIMPLE)} - {timeslot.end.toLocaleString(DateTime.TIME_SIMPLE)}
@@ -97,10 +99,10 @@ const Scheduler = ({ setSchedule, updateView, duration }) => {
                         }
                     </select>
                     <button onClick={onConfirm}>Continue</button>
-                </div>
-                
+                </div>             
             </div>
         </>
     )
 }
+
 export default Scheduler
