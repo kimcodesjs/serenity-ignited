3382e43df0429e556eb07aac457aaab4ad8523e3
Kim Duer <kimcodesjs@gmail.com>
2022-02-17 23:16:48 -0600

    implemented cancel appointment feature

--- src/Booking/AppointmentCard.js
+++ src/Booking/AppointmentCard.js
@@ -2,29 +2,24 @@ import React from 'react'
 import { createUseStyles } from 'react-jss'
 import { DateTime } from 'luxon'
 
-// const useStyles = createUseStyles({
-//     appointmentCard: {
-//         marginBottom: '20px',
-//         width: '350px',
-//         padding: '10px',
-//         background: 'radial-gradient(ellipse at top, rgba(130, 150, 188, .7), transparent), radial-gradient(ellipse at bottom, rgba(130, 150, 188, .7), transparent)',
-//         textShadow: '#e5d7d7 1px 0px 5px',
-//         filter: 'drop-shadow(2px 2px 1px #443356)',
-//     },
-//     h4: {
-//         margin: 0
-//     },
-// })
-// date and time props will be luxon objects
-const AppointmentCard = ({ session, connection, date, time, useStyles }) => {
+const currencyFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+})
+
+const AppointmentCard = ({ session, connection, date, time, useStyles, onCancel }) => {
 
     const classes = useStyles()
     
     return (
         <div className={classes.appointmentCard}>
-            <h4 className={classes.h4}>{session.modality}: {session.id}</h4>
+            <h3 className={classes.h4}>{session.modality}: {session.id}</h3>
             <h4 className={classes.h4}>{connection} on {date.toLocaleString(DateTime.DATE_HUGE)}</h4>
             <h4 className={classes.h4}>{time.start.toLocaleString(DateTime.TIME_SIMPLE)} to {time.end.toLocaleString(DateTime.TIME_SIMPLE)}</h4>
+            <h4 className={classes.h4}>{currencyFormatter.format(session.price)}</h4>
+            {onCancel && <button className={classes.button} onClick={onCancel}>Cancel</button>}
         </div>
     )
 }
--- src/MySessions.js
+++ src/MySessions.js
@@ -1,42 +1,50 @@
 import React, { useState, useEffect } from 'react'
 import AppointmentCard from './Booking/AppointmentCard'
 import { db } from './index'
-import { collection, query, where, getDocs, orderBy } from 'firebase/firestore'
+import { collection, query, where, getDocs, orderBy, deleteDoc, doc } from 'firebase/firestore'
 import { useParams } from 'react-router-dom'
 import { DateTime, Interval } from 'luxon'
 import { createUseStyles } from 'react-jss'
 
 const useStyles = createUseStyles({
     mySessions: {
-        //backgroundImage: 'linear-gradient(to right, transparent, rgba(232, 232, 185, .92) 10%, rgba(232, 232, 185, .92) 90%, transparent),url("/angel wings chakras 1.jpg")',
         background: 'radial-gradient(ellipse at top, rgba(232, 232, 185, .92), transparent), radial-gradient(ellipse at bottom, rgba(232, 232, 185, .92), transparent), url("/angel wings chakras 1.jpg")',
         backgroundAttachment: 'fixed',
         paddingTop: '50px',
         width: '100%',
-        height: '100%'
+        minHeight: '100%'
     },
     h2: {
         fontFamily: "'Over the Rainbow', cursive",
         fontSize: '40px',
-        //textAlign: 'center',
         marginTop: 0,
         marginLeft: '10%',
-        //marginRight: 'auto',
         width: '400px'
+    },
+    mySessionsContnent: {
+        display: 'inline-flex',
+        flexDirection: 'column',
+        width: '80%',
+        marginLeft: '10%',
+        marginRight: '10%'
     },
     apptCardContainer: {
         display: 'inline-flex',
         flexFlow: 'row wrap',
-        width: '80%',
-        marginLeft: '10%',
-        marginRight: '10%'
-    }
+        width: '50%',
+        
+    },
+    apptEditContainer: {
+        width: '50%',
+        background: 'radial-gradient(ellipse at top, rgba(130, 150, 188, .7), transparent), radial-gradient(ellipse at bottom, rgba(130, 150, 188, .7), transparent)', 
+    },
+    
 })
 
 const useAppointmentCardStyles = createUseStyles({
     appointmentCard: {
         margin: '20px',
-        width: '350px',
+        width: '400px',
         padding: '10px',
         background: 'radial-gradient(ellipse at top, rgba(130, 150, 188, .7), transparent), radial-gradient(ellipse at bottom, rgba(130, 150, 188, .7), transparent)',
         textShadow: '#e5d7d7 1px 0px 5px',
@@ -46,6 +54,19 @@ const useAppointmentCardStyles = createUseStyles({
     h4: {
         margin: 0
     },
+    button: {
+        marginRight: '25%',
+        marginTop: '20px',
+        width: '85px',
+        textAlign: 'center',
+        userSelect: 'none',
+        cursor: 'pointer',
+        background: 'radial-gradient(ellipse at top, rgba(130, 150, 188, .7), transparent), radial-gradient(ellipse at bottom, rgba(130, 150, 188, .7), transparent)',
+        textShadow: '#e5d7d7 1px 0px 5px',
+        filter: 'drop-shadow(2px 2px 1px #443356)',
+        borderRadius: '10px',
+        fontWeight: 'bold'
+    }
 })
 const MySessions = () => {
     
@@ -53,30 +74,49 @@ const MySessions = () => {
     const classes = useStyles()
 
     let { userID } = useParams()
+
+    
     useEffect(async () => {
         let appointments = []   
         await getDocs(query(collection(db, 'Appointments'), where('userID', '==', `${userID}`), orderBy('date'))).then((results) => {
             results.forEach((doc) => {
-                appointments.push(doc.data())
+                appointments.push({
+                    data: doc.data(),
+                    id: doc.id
+                })
             })
             setUserAppointments(appointments)
         })
     },[])
-    
+
+
+    const onCancel = async (appointmentID) => {
+        await deleteDoc(doc(db, 'Appointments', appointmentID)).then(() => {
+            setUserAppointments(userAppointments.filter((appointment) => {
+                if (appointment.id != appointmentID) return appointment
+            }))
+        })
+    }
+
     return (
         <div className={classes.mySessions}>
             <h2 className={classes.h2}>My Sessions</h2>
-            <div className={classes.apptCardContainer}>
-                {userAppointments.length > 0 && userAppointments.map((appointment) => {
-                    return <AppointmentCard 
-                                key={userAppointments.indexOf(appointment)} 
-                                session={appointment.session} connection={appointment.connection} 
-                                date={DateTime.fromISO(appointment.date)} 
-                                time={Interval.fromISO(appointment.timeISO)}
-                                useStyles={useAppointmentCardStyles}
-                                
-                            />
-                })}
+            <div className={classes.mySessionsContnent}>
+                <div className={classes.apptCardContainer}>
+                    {userAppointments.length > 0 && userAppointments.map((appointment) => {
+                        return <AppointmentCard 
+                                    key={appointment.id} 
+                                    session={appointment.data.session} connection={appointment.data.connection} 
+                                    date={DateTime.fromISO(appointment.data.date)} 
+                                    time={Interval.fromISO(appointment.data.timeISO)}
+                                    useStyles={useAppointmentCardStyles}
+                                    onCancel={(e) => {e.preventDefault(); onCancel(appointment.id)}}
+                                />
+                    })}
+                </div>
+                <div className={classes.apptEditContainer}>
+                    
+                </div>
             </div>
         </div>
     )
