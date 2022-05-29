094189e469e49076804e57a4f14a91450ced8801
Kim Duer <kimcodesjs@gmail.com>
2022-05-29 10:37:05 -0500

    about snippet

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -21,7 +21,7 @@ const useStyles = createUseStyles({
         position: 'absolute',
         top: 0,
         left: 0,
-        height: '100%',
+        height: '110%',
         width: '100%',
         objectFit: 'cover',
         zIndex: '-2'
@@ -30,7 +30,7 @@ const useStyles = createUseStyles({
         position: 'absolute',
         top: 0,
         left: 0,
-        height: '100%',
+        height: '110%',
         width: '100%',
         objectFit: 'cover',
         zIndex: '-1',
@@ -72,6 +72,58 @@ const useStyles = createUseStyles({
             width: '90%',
             height: 'auto'
         }
+    },
+    contentContainer: {
+        background: 'white',
+        maskImage: 'linear-gradient(transparent, black 10%, black 90%, white)',
+        height: '800px'
+    },
+    aboutSnippet: {
+        width: '70%',
+        paddingTop: '100px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        display: 'flex',
+        flexFlow: 'row wrap'
+    },
+    aboutTitle: {
+        width: '100%'
+    },
+    aboutText: {
+        width: '70%',
+        textAlign: 'justify'
+    },
+    aboutButton: {
+        width: '30%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        width: '140px',
+        height: '60px',
+        fontFamily: "'Clicker Script', cursive",
+        fontSize: '30px',
+        textShadow: '#e5d7d7 1px 0px 5px',
+        color: 'white',
+        background: 'radial-gradient(ellipse at top, rgba(64, 69, 178, .92), transparent), radial-gradient(ellipse at bottom, rgba(56, 17, 17, 1), transparent)',
+        borderRadius: '30px',
+        border: 'none',
+        filter: 'drop-shadow(2px 2px 1px #443356)',
+        cursor: 'pointer',
+        transition: 'opacity ease-in-out 1s',
+        '@media (max-width: 720px)': {
+            fontSize: '28px',
+            width: '140px',
+            height: '60px',
+            marginTop: '15px'
+        },
+        '@media (max-width: 300px)': {
+            fontSize: '20px',
+            width: '110px',
+            height: '40px',
+            marginTop: '15px'
+        },
     }
 })
 
@@ -99,9 +151,15 @@ const Landing = () => {
                     Let us walk with you on your journey to find peace, happiness, and serenity.
                 </p>
             </div>
-            <EnergyHealing />
-            <Reiki />
-            <Access />
+            <div className={classes.contentContainer}>
+                <div className={classes.aboutSnippet}>
+                    <h1 className={classes.aboutTitle}>Our Mission</h1>
+                    <h3 className={classes.aboutText}>Serenity Ignited, LLC is committed to helping people ignite the fire within themselves to move forward from past traumas and bring serenity to their lives. We empower people to stand in their own sovereignty, allowing them to embrace their true Self. We offer healing to help people access what they hold inside, meditations to help shape awareness of the world around us, a safe space to talk about it while they build their foundation, and tools that sustain them on their journey forward.</h3>
+                    <div className={classes.aboutButton}>
+                        <button className={classes.button}>Read More</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 
