4668a5a38bbf70e7404683498e417f6f95e7f30a
Kim Duer <kimcodesjs@gmail.com>
2023-01-15 20:59:48 -0600

    initializing backend

---
+++ server/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+
+// ROUTER IMPORTS
+const userRouter = require("./routes/userRouter");
+
+// GLOBAL MIDDLEWARE
+app.use(helmet());
+app.use(cors());
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+app.use(express.json());
+app.use(cookieParser());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+
+// ROUTES
+app.use("/api/v1/users", userRouter);
---
+++ server/routes/userRouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.post('/login', authController.login);
+router.post('/signup', authController.signup);
+router.post(
+  '/forgotPassword',
+  authController.forgotPassword
+);
+router.patch(
+  '/resetPassword/:token',
+  authController.resetPassword
+);
+
+module.exports = router;
