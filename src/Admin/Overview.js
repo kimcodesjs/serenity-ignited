ce6ffe810cb0227e4a7d94277660091d757e1962
Kim Duer <kimcodesjs@gmail.com>
2023-02-26 12:01:06 -0600

    fetch all appointments, display in admin frontend

--- src/Admin/Overview.js
+++ src/Admin/Overview.js
@@ -1,43 +1,50 @@
-import React, { useState } from 'react'
-import Calendar from 'react-calendar'
+import React, { useState, useEffect } from 'react';
+import Calendar from 'react-calendar';
+import axios from 'axios';
+import { DateTime } from 'luxon';
 
 const Overview = () => {
+  const [appointments, setAppointments] = useState(null);
 
-    const [appointments, getAppointments] = useState({
-        client: 'John Doe',
-        practitioner: 'Becky Kappell',
-        type: 'sample',
-        // time: Interval() -> see Luxon tuts saved in bookmark
-    })
+  useEffect(() => {
+    const fetchAppointments = async () => {
+      try {
+        await axios({
+          method: 'GET',
+          url: 'http://127.0.0.1:3000/api/v1/appointments/get-all-appointments',
+          withCredentials: true,
+        }).then((res) => {
+          setAppointments(res.data.data);
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAppointments();
+  }, []);
 
-    // useEffect(() => {
-    //     fetchAppointments()
-    // }),[]
+  // how will appointments be sorted?
 
-    // const fetchAppointments = () => {
-    //     const appointment = 
-    //     getAppointments(appointment)
-        
-    // }
-    
+  return (
+    <div>
+      <h2>Overview</h2>
+      <h3>Upcoming Appointments</h3>
+      <Calendar
+      //tileDisabled={getDisabledDates}
+      />
+      {appointments
+        ? appointments.map((appointment) => (
+            <div key={appointment._id}>
+              <p>
+                {`${appointment.user.firstName} ${appointment.user.lastName}, ${appointment.session.name}`}
+              </p>
+            </div>
+          ))
+        : null}
+      <br />
+      <p>Sales Data</p>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <h2>Overview</h2>
-            <h3>Upcoming Appointments</h3>
-            <Calendar 
-                //tileDisabled={getDisabledDates}
-                />
-            {appointments ? 
-                <div>
-                    <p>{appointments.client}, {appointments.practitioner}, {appointments.type} </p>
-                </div> : null}
-            <br />
-            <p>Sales Data</p>
-
-        </div>
-    )
-
-}
-
-export default Overview
+export default Overview;
