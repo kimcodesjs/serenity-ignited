493c375d26d96046d047f8d23ba4299515b6bbfa
Kim Duer <kimcodesjs@gmail.com>
2023-06-12 22:52:36 -0500

    breakout admin styles to reuse

---
+++ src/Admin/adminStyles.js
@@ -0,0 +1,102 @@
+const adminStyles = {
+  sectionContainer: {
+    width: '500px',
+    maxWidth: '80%',
+  },
+  sectionTitle: {
+    textAlign: 'center',
+  },
+  calendar: {
+    display: 'flex',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  formSection: {
+    display: 'flex',
+    flexFlow: 'row wrap',
+    alignItems: 'center',
+    width: '100%',
+    maxWidth: '400px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    border: '1px dotted gray',
+    borderRadius: '10px',
+    marginTop: '10px',
+    marginBottom: '10px',
+    backgroundColor: 'aliceblue',
+  },
+  blockedDates: {
+    width: '400px',
+  },
+  fsHeading: {
+    width: '400px',
+    margin: '10px 5px 0px 10px',
+  },
+  fsRow: {
+    width: '400px',
+    margin: '0px 10px 0px 10px',
+  },
+  h4: {
+    margin: '10px 5px 10px 10px',
+  },
+  label: {
+    fontWeight: 'bold',
+  },
+  formButton: {
+    borderRadius: '10px',
+    cursor: 'pointer',
+  },
+
+  ul: {
+    paddingInlineStart: '0px',
+    display: 'flex',
+    flexFlow: 'row wrap',
+    marginTop: '5px',
+    marginBottom: '5px',
+    marginLeft: '10px',
+  },
+  li: {
+    cursor: 'pointer',
+    backgroundColor: 'lightgray',
+    listStyleType: 'none',
+    width: 'fit-content',
+    borderRadius: '10px',
+    padding: '5px',
+    marginRight: '5px',
+    marginBottom: '5px',
+    select: 'none',
+  },
+  textInput: {
+    borderRadius: '10px',
+    padding: '5px',
+    margin: '10px',
+    borderWidth: '1px',
+    fontFamily: 'inherit',
+    width: '-webkit-fill-available',
+    width: '-moz-available',
+    width: 'fill-available',
+  },
+  numInput: {
+    borderRadius: '10px',
+    padding: '5px',
+    borderWidth: '1px',
+    fontFamily: 'inherit',
+    width: '50px',
+    margin: '10px',
+  },
+  select: {
+    fontFamily: 'Didact Gothic',
+    margin: '10px',
+    borderRadius: '10px',
+    padding: '5px',
+  },
+  submitButton: {
+    display: 'block',
+    borderRadius: '10px',
+    fontSize: '20px',
+    margin: '10px auto',
+    cursor: 'pointer',
+  },
+};
+
+export default adminStyles;
