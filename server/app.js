ed622c973568e8837c80a8d4c365a9d6428346f6
Kim Duer <kimcodesjs@gmail.com>
2023-09-13 05:15:52 -0500

    msc

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
-
+const compression = require('compression');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./utils/errorHandler');
 
@@ -16,6 +16,7 @@ const userRouter = require('./routes/userRouter');
 const sessionRouter = require('./routes/sessionRouter');
 const appointmentRouter = require('./routes/appointmentRouter');
 const eventRouter = require('./routes/eventRouter');
+const practitionerRouter = require('./routes/practitionerRouter');
 
 const app = express();
 
@@ -26,7 +27,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 const limiter = rateLimit({
-  max: 100,
+  max: 1000,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
 });
@@ -36,12 +37,14 @@ app.use(cookieParser());
 app.use(mongoSanitize());
 app.use(xss());
 app.use(hpp());
+app.use(compression());
 
 // ROUTES
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/sessions', sessionRouter);
 app.use('/api/v1/appointments', appointmentRouter);
 app.use('/api/v1/events', eventRouter);
+app.use('/api/v1/practitioners', practitionerRouter);
 
 app.use(globalErrorHandler);
 module.exports = app;
