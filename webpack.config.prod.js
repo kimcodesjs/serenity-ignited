8d2f7623d99cef0399abfaa0e86ec02aa3223c5b
Kim Duer <kimcodesjs@gmail.com>
2023-09-19 20:58:55 -0500

    add production webpack config file

---
+++ webpack.config.prod.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [new Dotenv()],
+  mode: 'production',
+  devtool: 'eval-cheap-module-source-map',
+};
