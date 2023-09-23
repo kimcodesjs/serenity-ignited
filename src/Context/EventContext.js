787cd3474c7448853f9260311605317ca47df1b3
Kim Duer <kimcodesjs@gmail.com>
2023-09-23 00:52:49 -0500

    added missing showAlert import

--- src/Context/EventContext.js
+++ src/Context/EventContext.js
@@ -1,5 +1,6 @@
 import React, { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
+import { showAlert } from '../Utilities/alert';
 
 const EventContext = createContext(null);
 
